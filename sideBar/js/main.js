e4c2138ad3f0c9351fedca44024f804871036943
yangfang <yangfang@qq.com>
2016-04-19 09:18:16 +0800

    sidebar

---
+++ sideBar/js/main.js
@@ -0,0 +1,12 @@
+/**
+ * Created by eva on 16/4/18.
+ */
+requirejs.config({
+    paths: {
+        jquery: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min'
+    }
+});
+
+requirejs(['jquery'],function($){
+
+});
