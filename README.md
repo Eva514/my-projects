94e732097ba98a764defdc58dc33024a8e0e3beb
yangfang <yangfang@qq.com>
2016-04-20 17:40:36 +0800

    ecard

--- README.md
+++ README.md
@@ -9,3 +9,6 @@ Apple watch宣传页面效果http://eva514.github.io/my-projects/fullpage-appleW
 sidebar
   侧栏工具条示例--三种方式实现，并学习了requirejs三种基本方法
   http://eva514.github.io/my-projects/sideBar/index.html
+
+e-card
+  HTML5+CSS3实现新春贺卡效果http://eva514.github.io/my-projects/e-card/index.html
--- e-card/index.js
+++ e-card/index.js
@@ -1,3 +1,54 @@
-/**
- * Created by eva on 16/4/20.
- */
+
+window.onload = function(){
+    var page1 = document.getElementById("page1");
+    var page2 = document.getElementById("page2");
+    var page3 = document.getElementById("page3");
+
+    var musicCtrl = document.getElementById("musicCtrl");
+    var audio = document.getElementsByTagName("audio")[0];
+
+    //音乐播放结束，光盘旋转效果自动停止
+    audio.addEventListener('ended',function(event){//ended是audio的一个API
+        musicCtrl.setAttribute('class','');
+        //musicCtrl.style.animationPlayState = "paused";
+    },false);
+
+    //点击音乐图标控制音乐播放效果
+    musicCtrl.addEventListener('touchstart',function(event){
+        if(audio.paused){
+            audio.play();
+            this.setAttribute('class','play');
+            //this.style.animationPlayState = "running";//兼容性太差，iPhone手机以及低版本安卓不兼容
+        }else{
+            audio.pause();
+            this.setAttribute('class','');
+            //this.style.animationPlayState = "paused";
+        }
+    },false);
+
+    /*
+    musicCtrl.onclick = function(){
+        if(audio.paused){
+            audio.play();
+            this.setAttribute('class','play');
+            //this.style.animationPlayState = "running";
+        }else{
+            audio.pause();
+            this.setAttribute('class','');
+            //this.style.animationPlayState = "paused";
+        }
+
+    }*/
+
+    page1.addEventListener('touchstart',function(event){
+        page1.style.display = "none";
+        page2.style.display = "block";
+
+        setTimeout(function(){
+            page2.setAttribute('class','page fadeOut');
+            page3.setAttribute('class','page fadeIn');
+            page3.style.display = "block";
+        }, 5500)
+    },false);
+
+};
